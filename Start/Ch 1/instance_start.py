f5807e2769ab642bcb7aa931da8e08cfe9456e38
sheheryarghaznavi <sheheryarghaznavi@yahoo.com>
2025-08-06 03:58:12 +0500

    ch1 instance completed

--- Start/Ch 1/instance_start.py
+++ Start/Ch 1/instance_start.py
@@ -5,21 +5,39 @@
 class Book:
     # the "init" function is called when the instance is
     # created and ready to be initialized
-    def __init__(self, title):
+    def __init__(self, title, author, pages, price):
         self.title = title
         # TODO: add properties
+        self.author = author
+        self.pages = pages
+        self.price = price
+        self.__secret = "This is a secret attribute"
 
     # TODO: create instance methods
 
+    def getPrice(self):
+        if hasattr(self, "_discount"):
+            return self.price - (self.price * self._discount)
+        return self.price
+    
+    def setDiscount(self, amount):
+        self._discount = amount
+        return self.__secret
+
 
 # TODO: create some book instances
-b1 = Book("War and Peace")
-b2 = Book("The Catcher in the Rye")
+b1 = Book("War and Peace", 'Peter', 1000, 50)
+b2 = Book("The Catcher in the Rye", 'Ryan', 300, 30)
 
 # TODO: print the price of book1
-
+print(b1.getPrice())
+print("\n")
 
 # TODO: try setting the discount
+print(b2.getPrice())
+print(b2.setDiscount(0.25))
+print(b2.getPrice())
+print(b2._Book__secret)
 
 
 # TODO: properties with double underscores are hidden by the interpreter
