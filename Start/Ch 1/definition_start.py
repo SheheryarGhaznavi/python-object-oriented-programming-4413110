e5a10ae676c763aebcebdc1b93dedf224226dcc5
sheheryarghaznavi <sheheryarghaznavi@yahoo.com>
2025-08-06 03:45:23 +0500

    ch1 definition completed

--- Start/Ch 1/definition_start.py
+++ Start/Ch 1/definition_start.py
@@ -4,8 +4,17 @@
 
 # TODO: create a basic class
 
+class Book:
+
+    def __init__(self, title):        
+        self.title = title
 
 # TODO: create instances of the class
 
+book1 = Book('Brave New World')
+book2 = Book('Week Daddy Strong Daddy')
 
 # TODO: print the class and property
+
+print(book1)
+print(book1.title)
