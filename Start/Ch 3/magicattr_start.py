173efc1276dfd030f7c9d45f1df610fa609491be
sheheryarghaznavi <sheheryarghaznavi@yahoo.com>
2025-08-07 09:25:28 +0500

    ch3 magicattr completed

--- Start/Ch 3/magicattr_start.py
+++ Start/Ch 3/magicattr_start.py
@@ -17,13 +17,34 @@ class Book:
 
     # TODO: __getattribute__ called when an attr is retrieved. Don't
     # directly access the attr name otherwise a recursive loop is created
+    def __getattribute__(self, name):
+
+        if name == "price":
+            temp_price = super().__getattribute__("price")
+            temp_discount = super().__getattribute__("_discount")
+            return temp_price - ( temp_price * temp_discount)
+        
+        return super().__getattribute__(name)
 
     # TODO: __setattr__ called when an attribute value is set. Don't set the attr
     # directly here otherwise a recursive loop causes a crash
+    def __setattr__(self, name, value):
+        
+        if name == "price":
+            if type(value) is not float:
+                raise ValueError("The 'price' must be a float")
+            
+        return super().__setattr__(name, value)
 
     # TODO: __getattr__ called when __getattribute__ lookup fails - you can
     # pretty much generate attributes on the fly with this method
+    def __getattr__(self, name):
+        return name + " not exists here in this class"
 
 
 b1 = Book("War and Peace", "Leo Tolstoy", 39.95)
 b2 = Book("The Catcher in the Rye", "JD Salinger", 29.95)
+
+b1.price = float(40)
+print(b1.price2)
+print(b1)
