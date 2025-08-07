b9c7a630b095db58610792c8b55cf3c1d8ab02e9
sheheryarghaznavi <sheheryarghaznavi@yahoo.com>
2025-08-07 06:05:38 +0500

    ch2 composition completed

--- Start/Ch 2/composition_start.py
+++ Start/Ch 2/composition_start.py
@@ -3,23 +3,56 @@
 
 
 class Book:
-    def __init__(self, title, price, authorfname, authorlname):
+    def __init__(self, title, price, author = None):
         self.title = title
         self.price = price
 
-        self.authorfname = authorfname
-        self.authorlname = authorlname
+        self.author = author
 
         self.chapters = []
 
-    def addchapter(self, name, pages):
-        self.chapters.append((name, pages))
+    def addChapter(self, chapter):
+        self.chapters.append(chapter)
+
+    def getPagesCount(self):
+        count = 0
+        for chapter in self.chapters:
+            count += chapter.pages
+        return count
 
 
-b1 = Book("War and Peace", 39.0, "Leo", "Tolstoy")
+class Author:
+    def __init__(self, fname, lname):
+        self.fname = fname
+        self.lname = lname
 
-b1.addchapter("Chapter 1", 125)
-b1.addchapter("Chapter 2", 97)
-b1.addchapter("Chapter 3", 143)
+    def __str__(self):
+        return f"Fname {self.fname} Lname {self.lname}"
+
+
+class Chapter:
+    def __init__(self, name, pages):
+        self.name = name
+        self.pages = pages
+
+
+# author = Author("Leo", "Tolstoy")
+# b1 = Book("War and Peace", 39.0, author)
+
+# chapter1 = Chapter("Chapter 1", 125)
+# chapter2 = Chapter("Chapter 2", 97)
+# chapter3 = Chapter("Chapter 3", 143)
+
+# b1.addChapter(chapter1)
+# b1.addChapter(chapter2)
+# b1.addChapter(chapter3)
+
+b1 = Book("War and Peace", 39.0, Author("Leo", "Tolstoy"))
+
+b1.addChapter( Chapter("Chapter 1", 125) )
+b1.addChapter( Chapter("Chapter 2", 97) )
+b1.addChapter( Chapter("Chapter 3", 143) )
 
 print(b1.title)
+print(b1.author)
+print(b1.getPagesCount())
